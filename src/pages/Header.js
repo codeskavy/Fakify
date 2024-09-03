cb0ca43d82df3cdb353578a037a65b762404304a
codeskavy <kanikaverma0510@gmail.com>
2024-09-03 12:42:21 +0530

    landing page

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -6,13 +6,15 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <h1>MARKUPX</h1>
+        <h1>DeFake.ai</h1>
       </div>
       <nav className="nav-links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
         <a href="#how-it-works">How it works</a>
-        <a href="#features">Features</a>
-        <a href="#blogs">Blogs</a>
-        <a href="#faqs">FAQs</a>
+        <a href="#contact">Contact
+          
+        </a>
         <button className="get-started">Get Started</button>
       </nav>
     </header>
